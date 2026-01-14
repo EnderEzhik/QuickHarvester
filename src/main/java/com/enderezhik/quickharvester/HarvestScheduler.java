c0e4c2425c079f43700c897c96e1c46da566e1a1
EnderEzhik <Achernov2007@gmail.com>
2026-01-14 14:35:46 +0300

    Удалены старые наработки

--- src/main/java/com/enderezhik/quickharvester/HarvestScheduler.java
+++
@@ -1,9 +0,0 @@
-package com.enderezhik.quickharvester;
-
-import java.util.HashMap;
-import java.util.UUID;
-
-public class HarvestScheduler {
-    public static final HashMap<UUID, HarvestTask> tasks = new HashMap<>();
-}
-
